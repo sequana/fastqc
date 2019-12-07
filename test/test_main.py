d361f44244cd929235314dd6ded12c899a667fbd
Thomas Cokelaer <cokelaer@gmail.com>
2019-12-07 12:47:51 +0100

    Compressing the data and add tests

--- test/test_main.py
+++ test/test_main.py
@@ -6,9 +6,10 @@ import sys
 
 
 sequana_path = easydev.get_package_location('sequana_fastqc')
-sharedir = os.sep.join([sequana_path , "sequana", 'data'])
+sharedir = os.sep.join([sequana_path , "sequana_pipelines", 'fastqc', 'data'])
 
 
+# 
 def test_standalone_subprocess():
     directory = tempfile.TemporaryDirectory()
     cmd = "sequana_pipelines_fastqc --input-directory {} "
@@ -23,3 +24,25 @@ def test_standalone_script():
     sys.argv = ["test", "--input-directory", sharedir, "--working-directory",
         directory.name, "--run-mode", "local", "--force"]
     m.main()
+
+
+def test_full():
+
+    with tempfile.TemporaryDirectory() as directory:
+        print(directory)
+        wk = directory
+
+        cmd = "sequana_pipelines_fastqc --input-directory {} "
+        cmd += "--working-directory {} --run-mode local --force"
+        cmd = cmd.format(sharedir, wk)
+        subprocess.call(cmd.split())
+
+        stat = subprocess.call("sh fastqc.sh".split(), cwd=wk)
+
+        assert os.path.exists(wk + "/index.html")
+        assert os.path.exists(wk + "/tree.html")
+        assert os.path.exists(wk + "/multiqc/multiqc_report.html")
+
+def test_version():
+    cmd = "sequana_pipelines_fastqc --version"
+    subprocess.call(cmd.split())
